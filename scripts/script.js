f220260971ee08c1ec42c6c18099c69af0f117cd
Bighnesh Sahoo <bighnesh2001@gmail.com>
2021-09-11 19:17:33 +0530

    added reset button functionality and some styling changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 let gridSize = 20;
 const flexContainer = document.querySelector(".flex-container");
+const resetButton = document.querySelector("#reset-button");
 
-
-for (let i = 0; i < gridSize; i++) {
-  let newRow = document.createElement("div");
-  newRow.classList.add("flex-item")
-  for (let j = 0; j < gridSize; j++) {
-    let newGridBlock = document.createElement("div");
-    newGridBlock.classList.add("grid-box");
-    newRow.appendChild(newGridBlock);
+function createGridBoard() {
+  for (let i = 0; i < gridSize; i++) {
+    let newRow = document.createElement("div");
+    newRow.classList.add("flex-item")
+    for (let j = 0; j < gridSize; j++) {
+      let newGridBlock = document.createElement("div");
+      newGridBlock.classList.add("grid-box");
+      newRow.appendChild(newGridBlock);
+    }
+    flexContainer.appendChild(newRow);
   }
-  flexContainer.appendChild(newRow);
 }
-const gridBlocks = document.querySelectorAll(".grid-box");
-addHoverEvent();
 
 // This function is used to change the colour of the grid boxes on hover.
 function changeColor(e) {
-  this.style.backgroundColor = "red";
+  this.style.backgroundColor = "#F6D167";
 }
 
 function addHoverEvent() {
@@ -32,24 +32,31 @@ function removeHoverEvent() {
   })
 }
 
-// This function is used to temporarily remove the hover event handler
-function tempRemoveHoverEvent() {
-  const shiftKeyHandler = window.addEventListener('keydown', (e) => {
+// This event handler is used to temporarily remove the hover event handler
+window.addEventListener('keydown', (e) => {
     if (e.key === 'Shift') {
       removeHoverEvent();
     }
-  })
-}
+  });
 
-function addHoverEventAgain() {
-  const shiftKeyHandler = window.addEventListener('keyup', (e) => {
+// This event handler is used to readd the hover event for the grid boxes.
+window.addEventListener('keyup', (e) => {
     console.log(e);
     if (e.key === 'Shift') {
       addHoverEvent();
     }
   });
-}
+
+
+resetButton.addEventListener('click', () => {
+  gridBlocks.forEach((gridBlock) => {
+    gridBlock.style.backgroundColor = "white";
+  })
+})
 
 // Main driver function calls
-tempRemoveHoverEvent();
-addHoverEventAgain();
+createGridBoard();
+const gridBlocks = document.querySelectorAll(".grid-box");
+addHoverEvent();
+// tempRemoveHoverEvent();
+// addHoverEventAgain();
